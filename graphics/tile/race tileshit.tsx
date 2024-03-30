c5da19c6ee67d649d98df3c8691121f533ac8cbb
Razkin1234 <120375659+Razkin1234@users.noreply.github.com>
2024-03-30 14:46:19 +0300

    tiles
    very nice tiles

---
+++ graphics/tile/race tileshit.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="race tileshit" tilewidth="64" tileheight="64" tilecount="50" columns="5">
+ <image source="‏‏tileshit.png" width="320" height="640"/>
+</tileset>
